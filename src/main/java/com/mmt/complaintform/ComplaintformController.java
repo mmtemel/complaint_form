d0c76112fdd3e7bc67c5e47991f2660b79c3b5ea
mtemel <mmtemel41@gmail.com>
2023-07-26 19:02:27 +0300

    web dir to same package

--- src/main/java/com/mmt/complaintform/ComplaintformController.java
+++ src/main/java/com/mmt/complaintform/ComplaintformController.java
@@ -1,4 +1,4 @@
-package com.mmt.complaintform.web;
+package com.mmt.complaintform;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
