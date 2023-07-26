a8fe5a03c8394dd9d16a99c45e10381455f7bb63
mtemel <mmtemel41@gmail.com>
2023-07-26 18:52:32 +0300

    rest controller

---
+++ src/main/java/com/mmt/complaintform/web/ComplaintformController.java
@@ -0,0 +1,13 @@
+package com.mmt.complaintform.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ComplaintformController {
+    
+    @RequestMapping("/welcome")
+    public String welcome(){
+        return "Welcome to Complaint Form";
+    }
+}
---
+++ src/test/java/com/mmt/complaintform/ComplaintformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmt.complaintform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
